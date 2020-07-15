0282bb01b423251df2414fa8011f262e5df69d83
RazvanDiaconu <62401110+RazvanDiaconu@users.noreply.github.com>
2020-07-15 17:12:24 +0300

    added server logic

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express= require("express")
+const express= require('express')
 const app=express()
-app.get("/",(request,response)=>{
-    response.send("hello world ")
+const articleRouter=require('./routes/articles')
+app.set('view engine','ejs')
+app.use('/articles',articleRouter)
+const articles=[{
+    title:'test',
+    createdAt:Date.now(),
+    description:'test description'
+}]
+app.get('/',(request,response)=>{
+    response.render('index',{articles:articles})
 
 })
 app.listen(5000)
